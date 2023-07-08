06a293aa671e5e2ee317aab9285a2847538d868f
Ricardo <ricardowoj@hotmail.com>
2023-07-08 22:48:18 +0100

    Correcao lang to US.

--- quasar.conf.js
+++ quasar.conf.js
@@ -84,7 +84,7 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
-      lang: 'pt-BR',
+      lang: 'en-US',
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
